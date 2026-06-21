8cacc40c3572228c0ce0ba6014759e7f6f35d125
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-06-21 16:17:45 +0530

    add: added code inside main.go

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"stock-service/internal/handler"
+	"stock-service/internal/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	err := repository.InitDB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.Default()
+
+	router.GET(
+		"/api/v1/candles",
+		handler.GetCandles,
+	)
+
+	router.Run(":8080")
+}
