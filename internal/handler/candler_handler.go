af4191adf1f7e1fd419aa2f56c5fb01a3552c313
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-06-21 16:20:00 +0530

    add: added code inside handler

---
+++ internal/handler/candler_handler.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"net/http"
+	"stock-service/internal/service"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetCandles(c *gin.Context) {
+
+	symbol := c.Query("symbol")
+	timeframe := c.Query("timeframe")
+
+	startStr := c.Query("start_date")
+	endStr := c.Query("end_date")
+
+	start, err := time.Parse(
+		"2006-01-02 15:04:05",
+		startStr,
+	)
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	end, err := time.Parse(
+		"2006-01-02 15:04:05",
+		endStr,
+	)
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	resp, err := service.AggregateCandles(
+		symbol,
+		timeframe,
+		start,
+		end,
+	)
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
