5af190608a7d19751324277b2ff8e869f27f37ab
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-06-21 16:24:24 +0530

    add: updated models code

--- internal/model/candle.go
+++ internal/model/candle.go
@@ -3,11 +3,18 @@ package model
 import "time"
 
 type Candle struct {
-	Symbol   string
-	DateTime time.Time
-	Open     float64
-	High     float64
-	Low      float64
-	Close    float64
-	Volume   int64
+	Symbol   string    `json:"symbol"`
+	DateTime time.Time `json:"datetime"`
+	Open     float64   `json:"open"`
+	High     float64   `json:"high"`
+	Low      float64   `json:"low"`
+	Close    float64   `json:"close"`
+	Volume   int64     `json:"volume"`
+}
+
+type CandleResponse struct {
+	Symbol    string   `json:"symbol"`
+	Timeframe string   `json:"timeframe"`
+	Candles   []Candle `json:"candles"`
+	Count     int      `json:"count"`
 }
